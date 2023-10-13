76cd97adc0cb56ef0ae65ac4bd380433e7a60fb3
hao.chen <hao.chen@kyligence.io>
2023-10-13 18:37:34 +0800

    AL-8813 Support removeIdentityExpression method in SubQueryRemoveRule.

--- core/src/main/java/org/apache/calcite/rel/rules/SubQueryRemoveRule.java
+++ core/src/main/java/org/apache/calcite/rel/rules/SubQueryRemoveRule.java
@@ -16,6 +16,9 @@
  */
 package org.apache.calcite.rel.rules;
 
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Lists;
 import org.apache.calcite.plan.RelOptRule;
 import org.apache.calcite.plan.RelOptRuleCall;
 import org.apache.calcite.plan.RelOptRuleOperand;
@@ -30,8 +33,10 @@ import org.apache.calcite.rel.core.Project;
 import org.apache.calcite.rel.core.RelFactories;
 import org.apache.calcite.rel.metadata.RelMetadataQuery;
 import org.apache.calcite.rex.LogicVisitor;
+import org.apache.calcite.rex.RexCall;
 import org.apache.calcite.rex.RexCorrelVariable;
 import org.apache.calcite.rex.RexInputRef;
+import org.apache.calcite.rex.RexLiteral;
 import org.apache.calcite.rex.RexNode;
 import org.apache.calcite.rex.RexShuttle;
 import org.apache.calcite.rex.RexSubQuery;
@@ -45,9 +50,6 @@ import org.apache.calcite.tools.RelBuilderFactory;
 import org.apache.calcite.util.ImmutableBitSet;
 import org.apache.calcite.util.Pair;
 import org.apache.calcite.util.Util;
-
-import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableSet;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -112,6 +114,7 @@ public abstract class SubQueryRemoveRule extends RelOptRule {
                     e);
             final Set<CorrelationId>  variablesSet =
                 RelOptUtil.getVariablesUsed(e.rel);
+            c = removeIdentityExpression(builder, c);
             final RexNode target = apply(e, variablesSet, logic, builder, 1,
                     builder.peek().getRowType().getFieldCount(),
                     c.getKind() == SqlKind.EQUALS && variablesSet.isEmpty());
@@ -422,6 +425,39 @@ public abstract class SubQueryRemoveRule extends RelOptRule {
       return RexUtil.eq(subQuery, this.subQuery) ? replacement : subQuery;
     }
   }
+
+  private static RexNode removeIdentityExpression(RelBuilder builder, RexNode c) {
+    if (c.getKind() == SqlKind.AND) {
+      // remove always true expression
+      RexCall andCall = (RexCall) c;
+      List<RexNode> optimizedOperands = Lists.newArrayList(andCall.getOperands());
+
+      for (RexNode rexNode : andCall.getOperands()) {
+        RexCall rexCall = (RexCall) rexNode;
+        if (rexCall.getKind() == SqlKind.EQUALS
+                && rexCall.getOperands().stream().allMatch(RexLiteral.class::isInstance)) {
+          RexLiteral leftLiteral = (RexLiteral) rexCall.getOperands().get(0);
+          RexLiteral rightLiteral = (RexLiteral) rexCall.getOperands().get(1);
+
+          if (leftLiteral.getValue().equals(rightLiteral.getValue())) {
+            optimizedOperands.remove(rexCall);
+          } else {
+            // when one expression in the AND statement is false, other expressions do not need to be evaluated.
+            optimizedOperands = Lists.newArrayList(builder.literal(false));
+            break;
+          }
+        }
+      }
+      // update condition RexNode
+      if (optimizedOperands.size() == 1) {
+        c = optimizedOperands.get(0);
+      } else {
+        c = andCall.clone(andCall.getType(), optimizedOperands);
+      }
+    }
+    return c;
+  }
+
 }
 
 // End SubQueryRemoveRule.java
